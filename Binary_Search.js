ac735394101f62c338ae2e431eef0a042fbf6c8b
Satyamrai8707 <Satyamrai8707@gmail.com>
2025-08-01 15:31:37 +0530

    binary_search

---
+++ Binary_Search.js
@@ -0,0 +1,62 @@
+const arr = [1, 2, 2, 2, 2, 4, 5, 5];
+const target = 2;
+
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+  let i = 1;
+  while (left <= right) {
+    console.log("Step number :", i++);
+    let mid = Math.floor(left + (right - left) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (target < arr[mid]) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return -1;
+}
+
+console.log(binarySearch(arr, target));
+
+function firstOccurence(arr, target) {
+  let left = 0,
+    right = arr.length - 1;
+  let found = -1;
+  while (left <= right) {
+    let mid = Math.floor(left + (right - left) / 2);
+    if (arr[mid] === target) {
+      found = mid;
+      right = mid - 1;
+    } else if (target < arr[mid]) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return found;
+}
+
+console.log(firstOccurence(arr, target));
+
+function lastOccurence(arr, target) {
+    let left = 0,
+      right = arr.length - 1;
+    let found = -1;
+    while (left <= right) {
+      let mid = Math.floor(left + (right - left) / 2);
+      if (arr[mid] === target) {
+        found = mid;
+        left = mid + 1;
+      } else if (target < arr[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+    return found;
+  }
+  
+  console.log(lastOccurence(arr, target));
